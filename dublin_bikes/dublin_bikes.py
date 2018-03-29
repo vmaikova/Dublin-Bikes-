f83ddbc8f2385a1365fd1e10a533fdf2572e3ea6
Brynjabergmann <brynja.halldorsdottir@ucdconnect.ie>
2018-03-29 17:25:25 +0100

    Added html, css and js and served it with Flask

--- dublin_bikes/dublin_bikes.py
+++ dublin_bikes/dublin_bikes.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import os 
+from flask import Flask, send_from_directory
+#from flask.helpers import make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    #return app.send_static_file("index.html")#("/static/index.html")
+    return send_from_directory("../static", "index.html")
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('../static/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('../static/css', path)
+
+
+#if __name__ == "__main__":
+port = int(os.environ.get("PORT", 5000))
+app.run(host='127.0.0.1', port=port, debug = True) #Has to be 127.0.0.1 on windows

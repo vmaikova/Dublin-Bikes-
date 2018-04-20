dfda4508de0a0a4211e6957a6e8c75d3bb5cd147
Brynjabergmann <brynja.halldorsdottir@ucdconnect.ie>
2018-04-20 04:15:24 +0100

    added graphing support for server

--- dublin_bikes/dublin_bikes.py
+++ dublin_bikes/dublin_bikes.py
@@ -32,6 +32,10 @@ def forecast():
 def icon():
     return send_from_directory('../static', 'favicon.ico')
 
+@app.route("/graphing/<path:path>")
+def graph(path):
+    return send_from_directory('../static/graphs', path)
+
 #if __name__ == "__main__":
 port = int(os.environ.get("PORT", 5000))
 app.run(host='127.0.0.1', port=port, debug = True) #Has to be 127.0.0.1 on windows
